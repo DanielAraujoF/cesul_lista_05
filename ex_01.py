09cd65a875cb3279633dd36cacbeb2d289832eff
Daniel Araujo <danielaraujo.github@gmail.com>
2023-04-19 20:43:06 -0300

    commit message

---
+++ ex_01.py
@@ -0,0 +1,8 @@
+# 1) Construa um programa que receba dois números do usuário, e retorne como saída a diferença entre o maior e o menor.
+
+x = (int(input("Informe um número: ")))
+y = (int(input("Informe mais um número: ")))
+
+if x > y:
+    diferenca = x - y
+    print(f"A diferença entre x e y é {diferenca:.0f} ")
