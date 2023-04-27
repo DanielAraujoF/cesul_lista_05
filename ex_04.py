5c628da8f24bd12bca3cbe59548f04deca12cc99
Daniel Araujo <danielaraujo.github@gmail.com>
2023-04-26 22:24:42 -0300

    commit message

--- ex_04.py
+++ ex_04.py
@@ -1,16 +1,16 @@
 # 4) Desenvolver um programa que receba como entrada três números. Determinar qual entre estes três números é o maior.
 
-x = int(input("Informe três números. Insira apenas o primeiro número pensado: "))
-y = int(input("Insira o segundo número: "))
-z = int(input("Insira o terceiro número: "))
+x = float(input("Informe três números. Insira apenas o primeiro número pensado: "))
+y = float(input("Insira o segundo número: "))
+z = float(input("Insira o terceiro número: "))
 
 if (x > y) and (x > z):
     maiorNumero = x
 elif (y > x) and (y > z):
     maiorNumero = y
-elif (z > x) and (z > y):
+else:
     maiorNumero = z
 
-print(f"O número {maiorNumero:.0f} é o maior dentre os três inseridos.")
+print(f"O número {maiorNumero:.1f} é o maior dentre os três inseridos.")
 
 print("Fim!")
--- ex_05.py
+++ ex_05.py
@@ -1,23 +1,21 @@
 # 5) Desenvolva um programa que receba três valores inteiros, e determine se o menor valor é par.
 
-numero1 = int(input("Insira um número: "))
-numero2 = int(input("Insira um segundo número: "))
-numero3 = int(input("Insira um terceiro número: "))
+x = int(input("Insira um número: "))
+y = int(input("Insira um segundo número: "))
+z = int(input("Insira um terceiro número: "))
 
-if numero1 < numero2 and numero1 < numero3:
-    if int(numero1 / 2):
-        print(f"O menor número é {numero1:.0f}, e ele é par")
-else:
-    print(f"O menor número é {numero1:.0f}, e ele é impar")
+menorNumero = 0
 
-if numero2 < numero1 and numero2 < numero3:
-    if int(numero2 / 2):
-        print(f"O menor número é {numero2:.0f}, e ele é par")
-else:
-    print(f"O menor número é {numero2:.0f}, e ele é impar")
+if x < y and x < z:
+    menorNumero = x
 
-if numero3 < numero1 and numero3 < numero2:
-    if int(numero3 / 2):
-        print(f"O menor número é {numero3:.0f}, e ele é par")
+elif y < x and y < z:
+    menorNumero = y
+
 else:
-    print(f"O menor número é {numero3:.0f}, e ele é impar")
+    menorNumero = z
+
+if menorNumero % 2 == 0:
+    print(f"O menor número é {menorNumero} e ele é par.")
+else:
+    print(f"O menor número é {menorNumero} e ele é impar.")
